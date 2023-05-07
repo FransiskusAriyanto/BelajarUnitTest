8dabdb37803fa453b1855c9981b01e86278317b0
Fransiskus Ariyanto <106706978+FransiskusAriyanto@users.noreply.github.com>
2023-05-07 20:51:37 +0700

    Initial commit

---
+++ BelajarUnitTest/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/sum", (int? n1, int? n2) => n1 + n2);
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,2 @@
+# BelajarUnitTest
+ Belajar Unit Test dengan MSTest pada asp.net core 6
